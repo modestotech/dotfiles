5bb1f76f3e8b2acd02acf2ffef43fe00a4745ece
Max Modesto Wallin <modestotechnology@gmail.com>
2019-07-25 02:02:23 +0200

    Some Fedora and Vim stuff

---
+++ fedora/installStuff.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Update dnf
+sudo dnf upgrade
+echo "Upgraded dnf"
+sleep 1
+
+# Utilities
+sudo dnf install git -y
+sudo dnf install htop -y
+sudo dnf install vim-enhanced vim-X11 -y
+sudo dnf install tmux -y
+sudo dnf install httpie -y # For http requests
+
+echo "Success! Utilities have been installed."
+sleep 1
+
+# Dev stuff
+sudo dnf install node -y
+
+echo "Success! Dev stuff has been installed."
+sleep 1
+
+# Apps
+sudo dnf install chromium
+# TODO VS Code
+# TODO Virtualbox
+
+echo "Success! Apps have been installed."
+sleep 1
+
+# ...and then.
+echo "Success! A lot of stuff has been installed."
